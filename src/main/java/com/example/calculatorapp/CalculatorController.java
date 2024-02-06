d1a053cf6484ecbecbac9580d77393462900af03
pai-puella <cat.dyshkant@yandex.ru>
2024-02-06 22:16:11 +0300

    Added RequestMapping in CalculatorController

--- src/main/java/com/example/calculatorapp/CalculatorController.java
+++ src/main/java/com/example/calculatorapp/CalculatorController.java
@@ -1,36 +1,38 @@
 package com.example.calculatorapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @GetMapping("/calculator")
+    @GetMapping
     public String welcome() {
         return "Добро пожаловать в калькулятор";
     }
 
-    @GetMapping("/calculator/plus")
+    @GetMapping("/plus")
     public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = num1 + num2;
         return num1 + " + " + num2 + " = " + result;
     }
 
-    @GetMapping("/calculator/minus")
+    @GetMapping("/minus")
     public String subtract(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = num1 - num2;
         return num1 + " - " + num2 + " = " + result;
     }
 
-    @GetMapping("/calculator/multiply")
+    @GetMapping("/multiply")
     public String multiply(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         int result = num1 * num2;
         return num1 + " * " + num2 + " = " + result;
     }
 
-    @GetMapping("/calculator/divide")
+    @GetMapping("/divide")
     public String divide(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
         if (num2 == 0) {
             throw new IllegalArgumentException("Деление на 0 недопустимо");
