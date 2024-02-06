dafbb6bcd3097190c56fb5bcc2745a158510cc43
pai-puella <cat.dyshkant@yandex.ru>
2024-02-06 22:28:01 +0300

    Added RequestMapping in CalculatorController, added CalculatorService

---
+++ src/main/java/com/example/calculatorapp/CalculatorService.java
@@ -0,0 +1,23 @@
+package com.example.calculatorapp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
